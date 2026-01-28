46f7d9a88f7abf754c47b16b772ada44d9e771fa
chunjiu <1614949142@qq.com>
2026-01-28 22:54:44 +0800

    feat: 提交会议室登录

---
+++ meeting-room-app/src/index.tsx
@@ -0,0 +1,35 @@
+import ReactDOM from 'react-dom/client'
+import './index.less'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { Register } from './page/register/Register.tsx'
+import { Login } from './page/login/Login.tsx'
+import { UpdatePassword } from './page/update_password/UpdatePassword.tsx'
+import { ErrorPage } from './page/error/ErrorPage.tsx'
+
+const routes = [
+  {
+    path: '/',
+    element: <div>index</div>,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: 'login',
+    element: <Login />,
+  },
+  {
+    path: 'register',
+    element: <Register />,
+  },
+  {
+    path: 'update_password',
+    element: <UpdatePassword />,
+  },
+]
+
+const router = createBrowserRouter(routes, {
+  basename: (window as any).__MICRO_APP_BASE_ROUTE__ || '/meeting-room',
+})
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
+root.render(<RouterProvider router={router} />)
